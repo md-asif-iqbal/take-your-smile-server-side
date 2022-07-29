e50fb970749975875e6c790640455e436187e35e
Abdullah <mdabdullahnoman777@gmail.com>
2022-07-29 17:23:25 +0600

    create post api recent event

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,27 +7,24 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
-
 // verify Authentication
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader.split(" ")[1];
   if (!authHeader) {
-    return res.status(404).send({message: "Unauthorize access"})
+    return res.status(404).send({ message: "Unauthorize access" });
   }
   if (token) {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
       if (err) {
-        return res.status(403).send({message: "Forbidden access"})
+        return res.status(403).send({ message: "Forbidden access" });
       }
- req.decoded= decoded;
- next();
-});
-}
-  
-}
+      req.decoded = decoded;
+      next();
+    });
+  }
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a16moha.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -39,42 +35,52 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const customerReviewsCollection = client.db("CustomerReviews").collection("reviews");
-    const RecentEventCollection = client.db("HomePageFeathers").collection("RecentEvents");
-    const SummeryCollection = client.db("HomePageFeathers").collection("Summery");
+    const customerReviewsCollection = client
+      .db("CustomerReviews")
+      .collection("reviews");
+    const RecentEventCollection = client
+      .db("HomePageFeathers")
+      .collection("RecentEvents");
+    const SummeryCollection = client
+      .db("HomePageFeathers")
+      .collection("Summery");
 
-
-  
-     //User Get
-     app.get('/user',verifyJWT,async(req, res) => {
+    //User Get
+    app.get("/user", verifyJWT, async (req, res) => {
       const users = await usersCollection.find({}).toArray();
       res.send(users);
-   });
+    });
     //User Insert/Update
-  app.put('/user/:email', async(req, res) => {
-    const email = req.params.email;
-    const user = req.body;
-    const role = {role: "user"}
-    
-    const filter = {email: email};
-    const options = { upsert: true };
-     const updateDoc = {
-      $set: user
-    };
-    const result = await usersCollection.updateOne(filter, updateDoc, options);
-    const token = jwt.sign(filter, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
-    res.send({result, token});
-  });
-          // Md Abdullah Vai start here 
-          // Get All Reviews from Customer Reviews collection
-     // Get All Reviews from Customer Reviews collection
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const role = { role: "user" };
 
-     app.get("/reviews", async (req, res) => {
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(filter, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ result, token });
+    });
+    // Md Abdullah Vai start here
+    // Get All Reviews from Customer Reviews collection
+    // Get All Reviews from Customer Reviews collection
+
+    app.get("/reviews", async (req, res) => {
       const query = req.body;
       const reviews = await customerReviewsCollection.find(query).toArray();
       res.send(reviews);
     });
-    
+
     // Post a reviews from Customer Reviews collection
 
     app.post("/reviews", async (req, res) => {
@@ -83,22 +89,32 @@ async function run() {
       res.send(review);
     });
 
-      // Get All Recent Event  from Home Pages Feathers collection
+    // Get All Recent Event  from Home Pages Feathers collection
 
-      app.get("/recentEvents", async (req, res) => {
-        const query = req.body;
-        const reviews = await RecentEventCollection.find(query).toArray();
-        res.send(reviews);
-      });
+    app.get("/recentEvents", async (req, res) => {
+      const query = req.body;
+      const reviews = await RecentEventCollection.find(query).toArray();
+      res.send(reviews);
+    });
+    
+    app.post("/recentEvents", async (req, res) => {
+      const query = req.body;
+      const review = await RecentEventCollection.insertOne(query);
+      res.send(review);
+    });
+    // Get All Recent Event  from Home Pages Feathers collection
 
-        // Get All Recent Event  from Home Pages Feathers collection
+    app.get("/summery", async (req, res) => {
+      const query = req.body;
+      const reviews = await SummeryCollection.find(query).toArray();
+      res.send(reviews);
+    });
 
-        app.get("/summery", async (req, res) => {
-          const query = req.body;
-          const reviews = await SummeryCollection.find(query).toArray();
-          res.send(reviews);
-        });
-
+    app.post("/recentEvents", async (req, res) => {
+      const query = req.body;
+      const review = await customerReviewsCollection.insertOne(query);
+      res.send(review);
+    });
   } finally {
   }
 }
@@ -110,8 +126,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Event app listening on port ${port}`);
 });
-
-
-
-
-
